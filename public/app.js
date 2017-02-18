920b92e6547541de9728b5f542ba55afea9b92fc
Blazej <bwielk@gmail.com>
2017-02-18 00:03:02 +0000

    Improved search engine

--- public/app.js
+++ public/app.js
@@ -1,38 +1,44 @@
+var populateList = function(spotifyData){
+  var display = document.getElementById('albums');
+  display.innerText = "";
+  spotifyData.forEach(function(album){
+    var pTag = document.createElement('p');
+    var link = document.createElement('a');
+    var hr = document.createElement('HR');
+    var detail = album.artists[0].name;
+    pTag.innerText = album.name + " by " + detail;
+    pTag.style.cssText = "text-decoration:none;font-family: Oswald;font-weight: bold;color: ivory;";
+    link.href = album.external_urls.spotify;
+    link.style.cssText = "text-decoration:none";
+    console.log(link.href);
+    pTag.appendChild(hr);
+    link.appendChild(pTag);
+    display.appendChild(link);
+  });
+}
+
+var newRequest = function(url, callback){
+  request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.onload = callback;
+  request.send();
+}
+
+var requestComplete = function(){
+  if(this.status != 200) return;
+  var jsonString = this.responseText;
+  var results = JSON.parse(jsonString);
+  var found = results.albums.items;
+  populateList(found);
+  console.log(found);
+}
 
 var app = function(){
-  var newRequest = function(url, callback){
-    request = new XMLHttpRequest();
-    request.open("GET", url);
-    request.onload = callback;
-    request.send();
-  }
-
-  var requestComplete = function(){
-    if(this.status != 200) return;
-    var jsonString = this.responseText;
-    var spotifyData = JSON.parse(jsonString);
-    populateList(spotifyData.albums.items);
-  }
-
-  var populateList = function(spotifyData){
-    var display = document.getElementById('albums');
-    spotifyData.forEach(function(album){
-      var pTag = document.createElement('p');
-      var link = document.createElement('a');
-      var hr = document.createElement('HR');
-      pTag.innerText = album.name;
-      pTag.style.cssText = "text-decoration:none;font-family: Oswald;font-weight: bold;color: ivory;";
-      link.href = album.external_urls.spotify;
-      link.style.cssText = "text-decoration:none";
-      console.log(link.href);
-      pTag.appendChild(hr);
-      link.appendChild(pTag);
-      display.appendChild(link);
-    });
-  }
-
-  var url = "https://api.spotify.com/v1/search?q=metal&type=album";
+  var searchInput = document.getElementById('search-query');
+  searchInput.onkeyup = function(){
+  var url = "https://api.spotify.com/v1/search?q=" + searchInput.value +"&type=album";
   newRequest(url, requestComplete);
+}
 }
 
 window.onload = app;
